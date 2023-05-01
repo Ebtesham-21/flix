62b131a0fcb3a0b612d1b31ae712ff6998c1586d
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-05-02 03:42:05 +0600

    first commit

---
+++ register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Reeceflix</title>
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
+   
+</head>
+<body>
+    <div class="signInContainer">
+        <div class="column">
+            
+        </div>
+        
+    </div>
+</body>
+</html>

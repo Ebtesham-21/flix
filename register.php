57cb06ae5efeea52919b7d2abb1aaa8ce5bfd211
Ebtesham-21 <ebteshamhossain98@gmail.com>
2023-05-02 03:55:02 +0600

    second

--- register.php
+++ register.php
@@ -9,11 +9,26 @@
    
 </head>
 <body>
-    <div class="signInContainer">
+<div class="signInContainer">
+
         <div class="column">
-            
+            <form method="POST">
+
+                <input type="text" name="firstName" placeholder="First name">
+                <input type="text" name="lastName" placeholder="Last name">
+                <input type="text" name="userName" placeholder="Username">
+                <input type="email" name="email" placeholder="Email">
+                <input type="email" name="email2" placeholder="Confirm email">
+                <input type="password" name="password" placeholder="Password">
+                <input type="password" name="password2" placeholder="Confirm password">
+                <input type="submit" name="submitPassword" value="SUBMIT">
+
+
+
+            </form>
+
         </div>
-        
-    </div>
+
+</div>
 </body>
 </html>
